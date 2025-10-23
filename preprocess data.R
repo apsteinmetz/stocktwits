7ef042f4020390c364b16b2c9da6833e4ca675bb
Art Steinmetz <apsteinmetz@yahoo.com>
2025-10-22 20:21:33 -0400

    get tickers

--- preprocess data.R
+++ preprocess data.R
@@ -8,5 +8,23 @@ sentiments <- sentiments |>
   select(-message_id)
 
 short_list <- sentiments |>
-  head(1000) |>
-  as_tibble()
+  head(1000)
+
+
+sentiments <- sentiments |>
+  mutate(symbol_list = gsub("\\[|\\]|'| ", "", symbol_list)) |>
+  collect() |>
+  separate_longer_delim(symbol_list, delim = ",") |>
+  as_duckplyr_df() |>
+  # retain only rows with valid symbols (A-Z, 1-5 characters)
+  filter(grepl("^[A-Z]{1,5}$", symbol_list))
+
+popular_tickers <- sentiments |>
+  summarise(.by = symbol_list, count = n()) |>
+  arrange(desc(count)) |>
+  filter(count >= 1000) |>
+  collect()
+
+tickers <- sentiments |>
+  distinct(symbol_list) |>
+  arrange(symbol_list)
